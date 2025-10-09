199b8b51c64a5ac8ae4583e4e1fd2a120773d555
felix <xpf14561456@163.com>
2025-10-09 11:07:06 +0800

    feat(create-thread): add CreateDemo2
    show how to create a threqd by implementing Runnable interface

---
+++ src/main/java/com/felix/multithread/basic/create/CreateDemo2.java
@@ -0,0 +1,36 @@
+package com.felix.multithread.basic.create;
+
+import com.felix.common.util.Print;
+import com.felix.common.util.ThreadUtils;
+
+
+/**
+ * @author felix
+ */
+public class CreateDemo2 {
+
+    private static final int MAX_TURN = 5;
+    private static int THREAD_NO = 1;
+
+    static class RunTarget implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < MAX_TURN; i++) {
+                Print.cfc(ThreadUtils.getCurThreadName() + "运行第" + (i + 1) + "轮。");
+            }
+            Print.cfc(ThreadUtils.getCurThreadName() + "运行结束。");
+        }
+    }
+
+
+     public static void main(String[] args) {
+        Thread thread = null;
+        // 主线程中顺序创建Thread，保证THREAD_NO的线程安全性
+        for (int i = 0; i < 2; i++) {
+            thread = new Thread(new RunTarget(), "DemoThread-" + THREAD_NO++);
+            thread.start();
+        }
+        Print.cfc(ThreadUtils.getCurThreadName() + "运行结束。");
+    }
+
+}
