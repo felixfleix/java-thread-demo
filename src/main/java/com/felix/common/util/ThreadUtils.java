d201a866e35039fdab95afe58c0681cdea69a94a
felix <xpf14561456@163.com>
2025-10-09 12:19:35 +0800

    feat(util): ThreadUtiils add sleepMilliSeconds method

--- src/main/java/com/felix/common/util/ThreadUtils.java
+++ src/main/java/com/felix/common/util/ThreadUtils.java
@@ -1,5 +1,7 @@
 package com.felix.common.util;
 
+import java.util.concurrent.locks.LockSupport;
+
 /**
  * @author felix
  */
@@ -12,6 +14,15 @@ public class ThreadUtils {
      */
     public static String getCurThreadName() {
         return Thread.currentThread().getName();
+    }
+
+    /**
+     * 使当前线程休眠指定的毫秒数
+     *
+     * @param milliSeconds 线程休眠的毫秒数
+     */
+    public static void sleepMilliSeconds(long milliSeconds) {
+        LockSupport.parkNanos(milliSeconds * 1000L * 1000L);
     }
 
 }
