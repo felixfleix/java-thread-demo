8912105663c25b3e7ca25bf2c2955c48be86f955
felix <xpf14561456@163.com>
2025-10-10 22:02:44 +0800

    feat(util): add getCurThreadId and getCurThread method

--- src/main/java/com/felix/common/util/ThreadUtils.java
+++ src/main/java/com/felix/common/util/ThreadUtils.java
@@ -17,6 +17,24 @@ public class ThreadUtils {
     }
 
     /**
+     * 获取当前线程的ID
+     *
+     * @return 当前线程的ID
+     */
+    public static Long getCurThreadId() {
+        return Thread.currentThread().threadId();
+    }
+
+    /**
+     * 获取当前线程的实例
+     *
+     * @return 当前线程的实例
+     */
+    public static Thread getCurThread() {
+        return Thread.currentThread();
+    }
+
+    /**
      * 使当前线程休眠指定的毫秒数
      *
      * @param milliSeconds 线程休眠的毫秒数
