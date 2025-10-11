f397008958293ed02eb1cea79dc51e5ade7f92a6
felix <xpf14561456@163.com>
2025-10-12 00:30:45 +0800

    feat(common): add common TargetTask
    using for other demos

---
+++ src/main/java/com/felix/common/task/TargetTask.java
@@ -0,0 +1,54 @@
+package com.felix.common.task;
+
+import com.felix.common.util.Print;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author felix
+ */
+public class TargetTask implements Runnable {
+
+    /**
+     * 线程编号
+     */
+    private static final AtomicInteger THREAD_NO = new AtomicInteger(0);
+
+    /**
+     * 线程休眠时间间隔
+     */
+    private static final int SLEEP_GAP = 3000;
+
+    /**
+     * 线程名称
+     */
+    private final String threadName;
+
+    public TargetTask() {
+        threadName = "TargetTask-" + THREAD_NO.getAndIncrement();
+    }
+
+    @Override
+    public void run() {
+        Print.tco("任务：" + threadName + " 开始执行");
+        try {
+            Thread.sleep(SLEEP_GAP);
+        } catch (InterruptedException e) {
+            Print.tco("任务：" + threadName + " 执行被中断");
+            e.printStackTrace();
+            return;
+        }
+        Print.tco("任务：" + threadName + " 执行完成");
+    }
+
+    public String getThreadName() {
+        return threadName;
+    }
+
+    @Override
+    public String toString() {
+        return "TargetTask{" +
+                "threadName='" + threadName + '\'' +
+                '}';
+    }
+}
