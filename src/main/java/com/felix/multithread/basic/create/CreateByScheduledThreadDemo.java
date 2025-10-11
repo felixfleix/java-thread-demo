44116ece6fc4cb5e728ddb86528345fc05a60c39
felix <xpf14561456@163.com>
2025-10-11 17:34:09 +0800

    feat(thread-pool): show how to use scheduled thread pool

---
+++ src/main/java/com/felix/multithread/basic/create/CreateByScheduledThreadDemo.java
@@ -0,0 +1,49 @@
+package com.felix.multithread.basic.create;
+
+import com.felix.common.util.Print;
+import com.felix.common.util.ThreadUtils;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author felix
+ */
+public class CreateByScheduledThreadDemo {
+
+    private static final int SLEEP_GAP = 500;
+
+    private static final AtomicInteger THREAD_NO = new AtomicInteger(0);
+
+    static class TargetTask implements Runnable {
+
+        private final String threadName;
+
+        public TargetTask() {
+            threadName = "TargetTask-" + THREAD_NO.incrementAndGet();
+        }
+
+        @Override
+        public void run() {
+            Print.tco("任务：" + threadName + "开始执行。");
+            ThreadUtils.sleepMilliSeconds(SLEEP_GAP);
+            Print.tco("任务：" + threadName + "执行完毕。");
+        }
+    }
+
+    public static void main(String[] args) {
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(2);
+        for (int i = 0; i < 2; i++) {
+            executorService.scheduleAtFixedRate(new TargetTask(), 0, SLEEP_GAP, TimeUnit.MILLISECONDS);
+        }
+
+        ThreadUtils.sleepSeconds(2);
+        executorService.close();
+
+        Print.tco(ThreadUtils.getCurThreadName() + " 主线程执行完毕。");
+    }
+
+}
