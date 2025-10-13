66ed604739c445c9036bc38e1bf3b48cf52ef5e0
felix <xpf14561456@gmail.com>
2025-10-14 00:35:59 +0800

    feat(thread-pool): add shutdown thread pool method
    show how to gracefully shut down the thread pool.

--- src/main/java/com/felix/common/util/ThreadUtils.java
+++ src/main/java/com/felix/common/util/ThreadUtils.java
@@ -1,5 +1,7 @@
 package com.felix.common.util;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 
 /**
@@ -50,6 +52,53 @@ public class ThreadUtils {
      */
     public static void sleepSeconds(long seconds) {
         LockSupport.parkNanos(seconds * 1000L * 1000L * 1000L);
+    }
+
+    /**
+     * 关闭线程池
+     *
+     * @param threadPool 线程池
+     */
+    public static void shutdownThreadPoolGracefully(ExecutorService threadPool) {
+        // 线程池为空或已终止，无需关闭
+        if (threadPool == null || threadPool.isTerminated()) {
+            return;
+        }
+        try {
+            threadPool.shutdown();
+        } catch (SecurityException e) {
+            // 安全异常，线程池可能已关闭或正在关闭
+            return;
+        }
+        try {
+            if (!threadPool.awaitTermination(60, TimeUnit.SECONDS)) {
+                // 线程池未在60秒内终止，强制关闭
+                threadPool.shutdownNow();
+                if (!threadPool.awaitTermination(60, TimeUnit.SECONDS)) {
+                    Print.tco("线程池任务未正常执行结束！");
+                }
+            }
+        } catch (InterruptedException e) {
+            // 线程池关闭过程中被中断，强制关闭
+            threadPool.shutdownNow();
+        }
+
+        // 线程池还未终止，循环1000次，每次等待10毫秒，检查是否终止
+        if (!threadPool.isTerminated()) {
+            try {
+                for (int i = 0; i < 1000; i++) {
+                    if (threadPool.awaitTermination(10, TimeUnit.MILLISECONDS)) {
+                        break;
+                    }
+                    threadPool.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                Print.cfo(e.getMessage());
+            } catch (Throwable e) {
+                Print.cfo(e.getMessage());
+            }
+
+        }
     }
 
 }
